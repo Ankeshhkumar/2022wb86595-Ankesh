91e48e753ae45b174ff74b6ec04f0e35630cbfb8
Ankeshhkumar <Ankeshhkumar18@gmail.com>
2026-09-26 17:54:00 +0530

    Test Jenkins Poll SCM

--- src/main/java/com/devops/App.java
+++ src/main/java/com/devops/App.java
@@ -5,6 +5,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello from DevOps Lab!");
         System.out.println("Continuous Integration using Jenkins");
+        System.out.println("Poll SCM Test - Version 3");
     }
 
     public static int add(int a, int b) {
